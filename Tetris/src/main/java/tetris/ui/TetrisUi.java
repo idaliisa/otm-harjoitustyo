cc63876eb276d4ec91cab9e7a582cb0d86b90a7c
idaliisa <idaliisa@gmail.com>
2018-04-10 23:54:07 +0300

    tetromino arvotaan

--- Tetris/src/main/java/tetris/ui/TetrisUi.java
+++ Tetris/src/main/java/tetris/ui/TetrisUi.java
@@ -1,6 +1,7 @@
 
 package tetris.ui;
 
+import java.util.Random;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -8,25 +9,26 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import tetris.domain.Border;
+import tetris.domain.Tetromino;
 import tetris.domain.TetrominoI;
+import tetris.domain.TetrominoJ;
+import tetris.domain.TetrominoL;
+import tetris.domain.TetrominoO;
+import tetris.domain.TetrominoS;
+import tetris.domain.TetrominoT;
+import tetris.domain.TetrominoZ;
 
 
 public class TetrisUi extends Application {
     public static int WIDTH = 360;
     public static int HEIGHT = 540;
+    private Tetromino tetromino = drawRandomTetromino();
     
     @Override
     public void start(Stage stage) throws Exception {
         BorderPane pane = new BorderPane();
         pane.setPrefSize(WIDTH, HEIGHT);
-        
-        Border bord = new Border(0,0);
-        pane.getChildren().add(bord.getPolygonLower());
-        pane.getChildren().add(bord.getPolygonLeft());
-        pane.getChildren().add(bord.getPolygonRight());
-        
-        TetrominoI tetromino = new TetrominoI(WIDTH/2, 0);
-        pane.getChildren().add(tetromino.getTetromino());
+        pane.getChildren().add(this.tetromino.getTetromino());
         
         Scene scene = new Scene(pane);
         
@@ -60,6 +62,26 @@ public class TetrisUi extends Application {
     public static void main(String[] args) {
         launch(args);
         System.out.println("Hello world");
+    }
+    
+    public Tetromino drawRandomTetromino() {
+        Random random = new Random();
+        int next = random.nextInt(7);
+        if (next == 1) {
+            return new TetrominoI(WIDTH/2, 0);
+        } else if (next == 2) {
+            return new TetrominoJ(WIDTH/2, 0);
+        } else if (next == 3) {
+            return new TetrominoL(WIDTH/2, 0);
+        } else if (next == 4) {
+            return new TetrominoO(WIDTH/2, 0);
+        } else if (next == 5) {
+            return new TetrominoS(WIDTH/2, 0);
+        } else if (next == 6) {
+            return new TetrominoT(WIDTH/2, 0);
+        } else {
+            return new TetrominoZ(WIDTH/2, 0);
+        }  
     }
     
 }
